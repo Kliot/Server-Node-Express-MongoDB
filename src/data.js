c286b81759fca6ede1bbd7770d4b2d3dfee29e26
Nataliia <kliotnata@gmail.com>
2020-07-20 16:11:10 +0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export default {
+  products: [
+    { "_id":"5ef344c2ace1d731ecde8080",
+      "title": "Fruit",
+      "desc": "Getting four to five servings of fruit per day can reduce your risk of heart disease, obesity, and type 2 diabetes.",
+      "category": "fruits",
+      "image": "/images/d2.jpg",
+      "price": 50,
+      "company": "Agro",
+      "rating": 8,
+      "numReviews": 0,
+      "countInStock": 0
+ },
+
+ {"_id":"5ef34471ace1d731ecde807f", 
+  "title": "Blueberry",
+      "desc": "A berry is a small, pulpy, and often edible fruit. Typically, berries are juicy, rounded, brightly colored, sweet, sour or tart, and do not have a stone or pit",
+      "category": "berries",
+      "image": "/images/d1.jpg",
+      "price": 60,
+      "company": "Agro",
+      "rating": 5,
+      "numReviews": 10,
+      "countInStock": 10
+ },
+  {"_id":"1", 
+  "title": "Blueberry",
+      "desc": "A berry is a small, pulpy, and often edible fruit. Typically, berries are juicy, rounded, brightly colored, sweet, sour or tart, and do not have a stone or pit",
+      "category": "berries",
+      "image": "/images/d1.jpg",
+      "price": 60,
+      "company": "Agro",
+      "rating": 5,
+      "numReviews": 10
+ },
+ {"title": "Cucumber",
+      "desc": "Cucumber (Cucumis sativus) is a widely-cultivated creeping vine plant in the Cucurbitaceae gourd family that bears cucumiform fruits, which are used as vegetables.",
+      "category": "vegetable",
+      "image": "/images/d2.jpg",
+      "price": 50,
+      "company": "Agro",
+      "rating": 8,
+      "numReviews": 0,
+      "countInStock": 5
+ }
+ ]
+}
--- src/index.js
+++ src/index.js
@@ -1,30 +1,70 @@
 import mongoose from "mongoose";
 import express from "express";
 import bodyParser from "body-parser";
+import config from './config';
+import { getToken } from './util';
+//import cors from 'cors';
+//import data from "./data";
 
 import ProductModel from "./models/Product";
 import ProductController from "./controllers/ProductController";
 const Product = new ProductController();
 
+import UserModel from "./models/User";
+import UserController from "./controllers/UserController";
+const User = new UserController();
+
+//const cors = cors();
+
+var cors = require('cors');
 
 const app = express();
-mongoose.connect("mongodb://localhost/store", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+
+mongoose.connect("mongodb+srv://admin:LzleozMrsHvfH5tL@cluster0-qofod.mongodb.net/<dbname>?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+//mongoose.connect("mongodb://localhost/store", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 app.post('/goods', Product.create);
 
-app.get('/goods', Product.index);
+app.get('/goods', cors(), Product.index);
 
-app.get('/goods/:id', Product.read);
+app.get('/goods/:id', cors(), Product.read);
 
 app.delete('/goods/:id', Product.delete);
 
 app.put('/goods/:id', Product.update);
 
+//app.post('/singin', cors(), User.singin);
 
-app.listen(3000, function() {
+app.post('/users', User.create);
+
+app.get('/users', User.index);
+
+app.post('/users/signin', (req, res) => {
+  const signinUser =  UserModel.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  if (signinUser) {
+    res.send({
+      _id: signinUser.id,
+      name: signinUser.name,
+      email: signinUser.email,
+      isAdmin: signinUser.isAdmin,
+      token: getToken(signinUser),
+    });
+  } else {
+    res.status(401).send({ message: 'Invalid Email or Password.' });
+  }
+});
+
+
+
+
+app.listen(8080, function() {
 	console.log("Start");
 })
 
